f05f39b7869067d56527e53a2663b9abd763c4fc
Gustaf Wallnerström <gustaf@Gustafs-MBP.lan>
2018-06-20 22:48:26 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.nio.charset.Charset;
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Terminal terminal = TerminalFacade.createTerminal(System.in,
+                System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        while(true){
+//Wait for a key to be pressed
+            Key key;
+            do{
+                Thread.sleep(5);
+                key =terminal.readInput();
+            }
+            while(key == null);
+            System.out.println(key.getCharacter()+ " " + key.getKind());
+        }
+    }
+}
