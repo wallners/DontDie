36977fdd3ce12b2a3bdc4e94b822c0ca9b5a101d
wallner@strom <wallner@strom.se>
2018-06-21 09:35:35 +0200

    Add switch case

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.terminal.Terminal;
+
 import java.nio.charset.Charset;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Terminal terminal = TerminalFacade.createTerminal(System.in,
                 System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
-        while(true){
+        while (true) {
 //Wait for a key to be pressed
             Key key;
-            do{
+            do {
                 Thread.sleep(5);
-                key =terminal.readInput();
+                key = terminal.readInput();
             }
-            while(key == null);
-            System.out.println(key.getCharacter()+ " " + key.getKind());
+            while (key == null);
+            switch (key.getKind()) {
+                case ArrowDown:
+                    break;
+                case ArrowUp:
+                    break;
+                case ArrowLeft:
+                    break;
+                case ArrowRight:
+                    break;
+            }
+            System.out.println(key.getCharacter() + " " + key.getKind());
         }
     }
 }
