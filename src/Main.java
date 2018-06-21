da2a3ea266c0345fcf74c0c54976dc402c1b69b2
Gabriela <g.saavedra.letelier@gmail.com>
2018-06-21 10:30:47 +0200

    add Monster class with constructor and getter add new Monster in Main

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         char [][] board = new char [20][20];
         Player player = new Player(10,10);
+        Monster monster1 = new Monster(0,0);
         moveCursor(player, terminal);
         terminal.enterPrivateMode();
         while (true) {
