0081f50b705308996f693bcd1d2aaa97938e9cdd
wallner@strom <wallner@strom.se>
2018-06-21 10:03:07 +0200

    Make x and y private, and add getters in Player class

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,17 @@
 public class Player {
-    public int [] x;
-    public int [] y;
+    private int [] x;
+    private int [] y;
 
     public Player(int[] x, int[] y) {
         this.x = x;
         this.y = y;
     }
 
+    public int[] getX() {
+        return x;
+        }
 
+    public int[] getY() {
+        return y;
+    }
 }
